8f9e866a9429445d725004c35fc0d50325654d26
tunglain2 <tunglain2@gmail.com>
2018-12-28 21:54:29 +0800

    homework2 continue ticket

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets:");
-        int tickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets:");
-        int roundTripTickets = scanner.nextInt();
-        Ticket ticket = new Ticket(tickets,roundTripTickets);
-        ticket.totalPrint();
+        int tickets = -1;
+        while (tickets != 0) {
+            System.out.print("Please enter number of tickets:");
+            tickets = scanner.nextInt();
+            if(tickets == -1) break;
+            System.out.print("How many round-trip tickets:");
+            int roundTripTickets = scanner.nextInt();
+            Ticket ticket = new Ticket(tickets, roundTripTickets);
+            ticket.totalPrint();
+        }
     }
 }
